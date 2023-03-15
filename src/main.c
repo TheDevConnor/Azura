2469d5b5cff0ef0442e7fef4c24bf1dd133ac17d
Connor <99085769+TheRealHiThere@users.noreply.github.com>
2023-03-15 13:08:14 -0400

    Added in Binary op

--- src/main.c
+++ src/main.c
@@ -6,17 +6,8 @@
 int main (int argc, const char*argv[]) {
     initVM();
 
-    Chunk chunk;
-    initChunk(&chunk);
 
-    int constant = addConstants(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-    writeChunk(&chunk, OP_NEGATE, 123);
 
-    writeChunk(&chunk, OP_RETRUN, 123);
-    disassembleChunk(&chunk, "test chunk");
-    interpret(&chunk);
     freeVM();
-    freeChunk(&chunk);
+    return 0;
 }
