0758bb7233aaa1e604f856fbe597c75cb84d90af
Connor <99085769+TheRealHiThere@users.noreply.github.com>
2023-03-17 22:47:42 -0400

    NIL_VALO fixed

--- src/value.h
+++ src/value.h
@@ -25,8 +25,8 @@ typedef struct {
 #define AS_NUMBER(value)  ((value).as.number)
 
 #define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
-#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = 0}})
-#define NIL_VAL           ((Value){VAL_NIL, {.number = value}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+#define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
 
 typedef struct {
     int capacity;
