1add921904b6580dec8e46149de5bf23057fbb3a
Connor <99085769+TheRealHiThere@users.noreply.github.com>
2023-03-17 22:39:55 -0400

    somewhat error free. weird in vim.c 96-101 & 87

--- src/compiler.c
+++ src/compiler.c
@@ -142,7 +142,7 @@ static void grouping() {
 
 static void number() {
     double value = strtod(parser.previous.start, NULL);
-    emitConstant(value);
+    emitConstant(NUMBER_VAL(value));
 }
 
 static void unary() {
@@ -153,6 +153,7 @@ static void unary() {
 
     // Emite te operation instrustion
     switch(operationType) {
+        case TOKEN_BANG: emitByte(OP_NOT); break;
         case TOKEN_MINUS: emitByte(OP_NEGATE); break;
         default: return; // Unreachable;
     }
@@ -164,6 +165,12 @@ static void binary() {
     parsePrecedence((Precedence)(rule->precedence + 1));
 
     switch (operationType) {
+        case TOKEN_BANG_EQUALS: emitBytes(OP_EQUAL, OP_NOT); break;
+        case TOKEN_EQUAL_EQUAL: emitByte(OP_EQUAL); break;
+        case TOKEN_GREATER: emitByte(OP_GREATER); break;
+        case TOKEN_GREATER_EQUAL: emitBytes(OP_LESS, OP_NOT); break;
+        case TOKEN_LESS: emitByte(OP_LESS); break;
+        case TOKEN_LESS_EQUAL: emitBytes(OP_GREATER, OP_NOT); break;
         case TOKEN_PLUS: emitByte(OP_ADD); break;
         case TOKEN_MINUS: emitByte(OP_SUBTRACT); break;
         case TOKEN_STAR: emitByte(OP_MULTIPLY); break;
@@ -171,6 +178,15 @@ static void binary() {
         default:
             break;
         }
+}
+
+static void literal() {
+    switch (parser.previous.type) {
+        case TOKEN_FALSE: emitByte(OP_FALSE); break;
+        case TOKEN_TRUE: emitByte(OP_TRUE); break;
+        case TOKEN_NIL: emitByte(OP_NIL); break;
+        default: return; // unreachable
+    }
 }
 
 ParseRule rules[] = {
@@ -185,32 +201,32 @@ ParseRule rules[] = {
     [TOKEN_SEMICOLON]   = {NULL,     NULL, PREC_NONE},
     [TOKEN_SLASH]       = {NULL,     binary, PREC_FACTOR},
     [TOKEN_STAR]        = {NULL,     binary, PREC_FACTOR},
-    [TOKEN_BANG]        = {NULL,     NULL, PREC_NONE},
-    [TOKEN_BANG_EQUALS] = {NULL,     NULL, PREC_NONE},
+    [TOKEN_BANG]        = {unary,     NULL, PREC_NONE},
+    [TOKEN_BANG_EQUALS] = {NULL,     NULL, PREC_EQUALITY},
     [TOKEN_WALRUS]      = {NULL,     NULL, PREC_NONE},  
     [TOKEN_EQUAL]       = {NULL,     NULL, PREC_NONE},
-    [TOKEN_EQUAL_EQUAL] = {NULL,     NULL, PREC_NONE},
-    [TOKEN_GREATER]     = {NULL,     NULL, PREC_NONE},
-    [TOKEN_GREATER_EQUAL] = {NULL,     NULL, PREC_NONE},
-    [TOKEN_LESS]        = {NULL,     NULL, PREC_NONE},
-    [TOKEN_LESS_EQUAL]  = {NULL,     NULL, PREC_NONE},
+    [TOKEN_EQUAL_EQUAL] = {NULL,     NULL, PREC_EQUALITY},
+    [TOKEN_GREATER]     = {NULL,     NULL, PREC_COMPARASION},
+    [TOKEN_GREATER_EQUAL] = {NULL,     NULL, PREC_COMPARASION},
+    [TOKEN_LESS]        = {NULL,     NULL, PREC_COMPARASION},
+    [TOKEN_LESS_EQUAL]  = {NULL,     NULL, PREC_COMPARASION},
     [TOKEN_IDENTIFIER]  = {NULL,     NULL, PREC_NONE},
     [TOKEN_STRING]      = {NULL,     NULL, PREC_NONE},
     [TOKEN_NUMBER]      = {number,   NULL, PREC_NONE},
     [TOKEN_AND]         = {NULL,     NULL, PREC_NONE},
     [TOKEN_CLASS]       = {NULL,     NULL, PREC_NONE},
     [TOKEN_ELSE]        = {NULL,     NULL, PREC_NONE},
-    [TOKEN_FALSE]       = {NULL,     NULL, PREC_NONE},
+    [TOKEN_FALSE]       = {literal,  NULL, PREC_NONE},
     [TOKEN_FOR]         = {NULL,     NULL, PREC_NONE},
     [TOKEN_FUNC]        = {NULL,     NULL, PREC_NONE},
     [TOKEN_IF]          = {NULL,     NULL, PREC_NONE},
     [TOKEN_INFO]        = {NULL,     NULL, PREC_NONE},
-    [TOKEN_NIL]         = {NULL,     NULL, PREC_NONE},
+    [TOKEN_NIL]         = {literal,     NULL, PREC_NONE},
     [TOKEN_OR]          = {NULL,     NULL, PREC_NONE},
     [TOKEN_RETURN]      = {NULL,     NULL, PREC_NONE},
     [TOKEN_SUPER]       = {NULL,     NULL, PREC_NONE},
     [TOKEN_THIS]        = {NULL,     NULL, PREC_NONE},
-    [TOKEN_TRUE]        = {NULL,     NULL, PREC_NONE},
+    [TOKEN_TRUE]        = {literal,     NULL, PREC_NONE},
     [TOKEN_VAR]         = {NULL,     NULL, PREC_NONE},
     [TOKEN_WHILE]       = {NULL,     NULL, PREC_NONE},
     [TOKEN_ERROR]       = {NULL,     NULL, PREC_NONE},
