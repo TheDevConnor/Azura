cb97bba062eb1431f84922fb520fdcae4f9bca24
Connor <99085769+TheRealHiThere@users.noreply.github.com>
2023-03-22 11:44:50 -0400

    Added in local variables. Next i need to add in a way to use the locals

--- src/compiler.c
+++ src/compiler.c
@@ -158,7 +158,15 @@ static void endCompiler() {
 
 static void beginScope() { current->scopeDepth++; }
 
-static void endScope() { current->scopeDepth--; }
+static void endScope() {
+  current->scopeDepth--;
+
+  while (current->localCount > 0 &&
+         current->locals[current->localCount - 1].depth) {
+    emitByte(OP_POP);
+    current->localCount--;
+  }
+}
 
 static void expression();
 static void statement();
@@ -172,12 +180,58 @@ static uint8_t identifierConstant(Token *name) {
   return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
 }
 
+static bool identifierEquals(Token *a, Token *b) {
+  if (a->length != b->length)
+    return false;
+  return memcmp(a->start, b->start, a->length) == 0;
+}
+
+static void addLocal(Token name) {
+  if (current->localCount == UINT8_COUNT) {
+    error("Too many local variables in the function!");
+    return;
+  }
+
+  Local *local = &current->locals[current->localCount++];
+  local->name = name;
+  local->depth = current->scopeDepth;
+}
+
+static void declareVariable() {
+  if (current->scopeDepth == 0)
+    return;
+
+  Token *name = &parser.previous;
+
+  for (int i = current->localCount - 1; i >= 0; i++) {
+    Local *local = &current->locals[i];
+    if (local->depth != -1 && local->depth < current->scopeDepth) {
+      break;
+    }
+
+    if (identifierEquals(name, &local->name)) {
+      error("Already a variable with this name declared in the scope!");
+    }
+  }
+
+  addLocal(*name);
+}
+
 static uint8_t parserVaruable(const char *errorMessage) {
   consume(TOKEN_IDENTIFIER, errorMessage);
+
+  declareVariable();
+  if (current->scopeDepth > 0)
+    return 0;
+
   return identifierConstant(&parser.previous);
 }
 
 static void defineVariable(uint8_t global) {
+  if (current->scopeDepth > 0) {
+    return;
+  }
+
   emitBytes(OP_DEFINE_GLOBAL, global);
 }
 
